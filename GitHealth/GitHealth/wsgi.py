b6272ececc28db867d3c7d158655978d6330c142
Ashkeelun <cbane@unomaha.edu>
2017-01-30 13:44:09 -0600

    Django Project Creation
    Created django project.

---
+++ GitHealth/GitHealth/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GitHealth project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GitHealth.settings")
+
+application = get_wsgi_application()
