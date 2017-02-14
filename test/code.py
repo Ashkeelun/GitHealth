2dcfa3fc05f682fa5b85ea228526eeed844ecbb6
Celal Gorgun <cgorgun@unomaha.edu>
2017-02-13 23:06:55 -0600

    Download DoSOCSv2 as a zip file

--- test/code.py
+++ test/code.py
@@ -10,6 +10,7 @@ for repo in g.get_user().get_repos():
 #Search Repository
 r = g.legacy_search_repos("DoSOCSv2")[0]
 #Url and owner name. 
-print r.url
+print r.url  
 print r.owner
-
+print r.get_archive_link("zipball") #get archive link chose zip format
+urllib.urlretrieve(r.get_archive_link("zipball"), r.name + ".zip") #this download as a zip file and save zip file name same as file that we try to download.
