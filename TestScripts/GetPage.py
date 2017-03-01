dae75444a04577a2bb58ff94e948f2643e591b6c
Ashkeelun <cbane@unomaha.edu>
2017-03-01 10:37:34 -0600

    Reverted to web scraping design
    Reverted to web scraping design and fixed multi-line detection. Repo URL will be obtained from GHdata which gets it from GHtorrent. So there's no need to use Github API.

--- TestScripts/GetPage.py
+++ TestScripts/GetPage.py
@@ -1,21 +1,30 @@
-import base64
-import urllib
-import os
+import requests
 import re
 
-from github import Github
+
+def rm_html_escapes(html):
+    html_escapes = [('&#39;', "'"), ('&quot;', '"'), ('&gt;', '>'), ('&lt;', '<'), ('&amp;', '&')]
+    for escape in html_escapes:
+        html = html.replace(escape[0], escape[1])
+    return html
+
+
+def get_file(ref, name, ext):
+    file_page = requests.get("https://github.com" + ref).text
+    raw_line_re = re.compile(r'<td.*?class=".*?js-file-line.*?".*?>(.*?)<\/td>')
+    raw_lines = '\n'.join(re.findall(raw_line_re, file_page))
+    rm_span_re = re.compile(r'(?:<span.*?>|<\/span>)')
+    lines = rm_html_escapes(re.sub(rm_span_re,'',raw_lines))
+    # print('\nFile Name: '+name+ext+'\n----------------------------------------\n\n'+lines+'\n\n')
+    return (name, ext, lines,)
+
 
 def parse_file_str(lines):
     slc_re = re.compile(r'#.*')
-    mlc_re = re.compile(r"'''((?:.|\n)*?)'''")
-    alc_re = re.compile(r"(?:'''(?:.|\n)*?'''|#.*)")
-    
+    mlc_re = re.compile(r'["\']{3}((?:.|\n)*?)["\']{3}')
+    alc_re = re.compile(r'(?:["\']{3}((?:.|\n)*?)["\']{3}|#.*)')
     slcs = re.findall(slc_re, lines)
     mlcs = re.findall(mlc_re, lines)
-
-    #print mlcs
-
-
     code = re.sub(alc_re, '', lines)
     comt_len = len(''.join(slcs)+''.join(mlcs))
     code_len = len(code)
@@ -23,37 +32,21 @@ def parse_file_str(lines):
     meta = [len(slcs), len(mlcs), len(''.join(slcs)), len(''.join(mlcs)), comt_len, code_len]
     return meta
 
-def analyze_dirs(directories):
-    if(len(directories) == 0):
-	return
 
-    metrics = []
+req = requests.get("https://github.com/OSSHealth/ghdata/tree/dev")
+r = req.text
+dir_re = re.compile(r'<a.*?href="(.+?)".*?class="js-navigation-open".*?>(?:<span.*?<\/span>)?(.+?)(\..*)?<\/a>')
+dirs = re.findall(dir_re, r)
+metrics = []
 
-    for dir in directories:
-        filename, file_extension = os.path.splitext(dir.name)
-        #print 'Filename: %-30s Ext: %-12s Type: %-10s Path: %-10s' %(filename, file_extension, dir.type, dir.path)
-
-    for dir in directories:
-
-        filename, file_extension = os.path.splitext(dir.name)
-	#print "DIR NAME: " + dir.name
-	#print "DIR TYPE: " + dir.type
-
-        if dir.type == "dir":
-	    #print "\n\nANALYZING:" + dir.name
-	    #print dir
-            analyze_dirs(r.get_dir_contents(dir.path, "dev"))
-	    #print "BACK TO : " + dir.name
-        elif dir.type == "file":
-
-            if dir.name.endswith(".py"):
-                decoded_file_content = base64.b64decode(dir.content)
-
-                m = parse_file_str(decoded_file_content)
-
-                metrics.append({'name': dir.name,
-                        'ext': file_extension,
-                        'url': dir.url,
+for dir in dirs:
+    print(dir[1] + "  " + dir[2] + " - " + dir[0])
+    if dir[2] == '.py':
+        f = get_file(dir[0], dir[1], dir[2])
+        m = parse_file_str(f[2])
+        metrics.append({'name': dir[1],
+                        'ext': dir[2],
+                        'url': dir[0],
                         'number of line Comments': m[0],
                         'number of multi-line Comments': m[1],
                         'size of line comments': m[2],
@@ -61,29 +54,8 @@ def analyze_dirs(directories):
                         'size of Comments': m[4],
                         'size of Code': m[5]
                         })
-
-        for file in metrics:
-            for m in file:
-                print("{}: {}".format(m, file[m]))
-            print("\n")
-
-g = Github("githealthtestuser","pass1234!")
-
-for repo in g.get_user().get_repos():
-    #File in githealthtestuser
-    print repo.name
-    repo.edit(has_wiki=False)
-
-#Search Repository
-r = g.legacy_search_repos("ghdata")[0]
-#Url and owner name. 
-print r.url
-print r.owner
-print r.get_archive_link("zipball")
-
-# print "\n\nANALYZING:root" 
-
-dirs = r.get_dir_contents("", "dev")
-analyze_dirs(dirs)
-
-# urllib.urlretrieve(r.get_archive_link("zipball"), r.name + ".zip")
+print("\n")
+for file in metrics:
+    for m in file:
+        print("{}: {}".format(m, file[m]))
+    print("\n")
