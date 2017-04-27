6621c2ea8a28e9bb33d69c2091c6676a976625c9
Celal Gorgun <cgorgun@unomaha.edu>
2017-04-26 23:37:45 -0500

    Update settings.py

--- GitHealth/GitHealth/settings.py
+++ GitHealth/GitHealth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y*6j5*l)nexkr-tnrej4hl^cgk%gvjhiyrf4+%1(%!d5gps($)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
